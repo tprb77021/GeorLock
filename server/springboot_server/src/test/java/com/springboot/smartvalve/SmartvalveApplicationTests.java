075771a68c111c532e2c4b2d56437ba487ab88d7
박태준 <taizoon72@gmail.com>
2020-07-17 14:13:55 +0900

    백 서버 <Frist>, maven

---
+++ server/springboot_server/src/test/java/com/springboot/smartvalve/SmartvalveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.smartvalve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartvalveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
