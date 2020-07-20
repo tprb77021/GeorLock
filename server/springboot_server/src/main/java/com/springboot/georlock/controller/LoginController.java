ddf75b806ecf42d10dce624567b04c4755c581b3
임세규 <tprb77021@gmail.com>
2020-07-20 12:12:02 +0900

    로그인쿠기(자동로그인)

--- server/springboot_server/src/main/java/com/springboot/georlock/controller/LoginController.java
+++ server/springboot_server/src/main/java/com/springboot/georlock/controller/LoginController.java
@@ -4,6 +4,11 @@ package com.springboot.georlock.controller;
 import com.springboot.georlock.dto.Test;
 import com.springboot.georlock.svc.LoginService;
 import com.springboot.georlock.svc.TestService;
+import org.apache.coyote.Response;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,19 +27,40 @@ public class LoginController {
 
 
     @RequestMapping("/loginform")
-    public String test() {
+    public ModelAndView loginform(HttpServletResponse response, HttpServletRequest request) throws Exception {
+        Cookie[] cookies = request.getCookies();
+        ModelAndView modelAndView = new ModelAndView("login");
+        if(cookies!=null){
+            String empNo="";
+            String userPw="";
+            for(int i=0; i<cookies.length; i++){
+                if(cookies[i].getName().equals("empNo")){
+                    empNo= cookies[i].getValue();
+                }
+                else  if(cookies[i].getName().equals("userPw")){
+                    userPw= cookies[i].getValue();
+                }
+            }
+            if(loginService.Login(empNo,userPw)){
+                modelAndView.setViewName("access");
+            }
+        }
         System.out.println("loginForm");
-        return "login";
+        return modelAndView;
     }
 
     @RequestMapping("/login")
-    public ModelAndView login(String empNo,String userPw) throws Exception{
+    public ModelAndView login(String empNo,String userPw,boolean autoLogin,HttpServletResponse response) throws Exception{
         System.out.println("login strat");
         ModelAndView modelAndView = new ModelAndView("access");
         if(!loginService.Login(empNo,userPw)){
             modelAndView.setViewName("login");
             modelAndView.addObject("message", "로그인 실패");
         }
+        if(autoLogin){
+            loginService.autoLogin(empNo,userPw,response);
+        }
+
         System.out.println("login end");
         return modelAndView;
     }
@@ -55,8 +81,16 @@ public class LoginController {
 
 
     @RequestMapping("/logout")
-    public String logout() {
+    public String logout(HttpServletResponse response, HttpServletRequest request) {
         System.out.println("logout");
+        Cookie[] cookies = request.getCookies();
+        if(cookies!=null){
+            for(int i=0; i<cookies.length; i++){
+                cookies[i].setMaxAge(0);
+                cookies[i].setPath("/");
+                response.addCookie(cookies[i]);
+            }
+        }
         return "redirect:/loginform";
 
     }
--- server/springboot_server/src/main/java/com/springboot/georlock/svc/LoginService.java
+++ server/springboot_server/src/main/java/com/springboot/georlock/svc/LoginService.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @Service
@@ -19,12 +21,24 @@ public class LoginService {
     public boolean Login(String empNo,String userPw) throws Exception{
         boolean loginsuccess= false;
         Login login=new Login();
-                login.setEmpNo(empNo);
-                login.setUserPw(userPw);
+        login.setEmpNo(empNo);
+        login.setUserPw(userPw);
       if(loginMapper.Login(login)!=null){
           loginsuccess=true;
       }
 
         return loginsuccess;
+    }
+
+    public void autoLogin(String empNo, String userPw,  HttpServletResponse response) {
+        Cookie cookie1 = new Cookie("empNo", empNo);
+        cookie1.setMaxAge(-1);
+        cookie1.setPath("/");
+        response.addCookie(cookie1);
+        Cookie cookie2 = new Cookie("userPw", userPw);
+        cookie2.setMaxAge(-1);
+        cookie2.setPath("/");
+        response.addCookie(cookie2);
+
     }
 }
